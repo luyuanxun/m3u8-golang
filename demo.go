1b337dfee16746ae68a6855083d851f7bf165af9
luyuanxun <904734889@qq.com>
2019-02-26 15:15:07 +0800

    Create demo.go
    协程demo

---
+++ demo.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+
+func main() {
+	//开启读-协程个数
+	chanNum := 10
+	//放置需要处理数据的chan（读写）
+	c := make(chan int, 100)
+	//标识是否已处理完并退出主线程
+	chanExit := make(chan int, 1)
+	go write(c)
+	time.Sleep(time.Second)
+	for i := 1; i <= chanNum; i++ {
+		fmt.Println("启动协程~：", i)
+		go read(c, chanExit, i)
+	}
+
+	//主线程等待结束(chan阻塞)
+	for {
+		_, ok := <-chanExit
+		if ok {
+			break
+		}
+	}
+
+	fmt.Println("main执行完成")
+}
+
+func write(c chan int) {
+	for i := 0; i < 10000; i++ {
+		fmt.Println("写入数据：", i)
+		c <- i
+	}
+
+	fmt.Println("写入数据完成")
+	close(c)
+}
+
+func read(c chan int, chanExit chan int, chanNum int) {
+	for {
+		v, ok := <-c
+		fmt.Printf("chan 取值：%d，协程：%d \n", v, chanNum)
+		if !ok {
+			break
+		}
+	}
+
+	fmt.Println("协程读取数据完成:", chanNum)
+	//读取完成
+	chanExit <- chanNum
+}
